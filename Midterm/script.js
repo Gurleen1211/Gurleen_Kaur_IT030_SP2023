f81871742f0c5ef03ac9f11e29c9e7aac17c582a
Gurleen1211 <gurleenkw1211@gmail.com>
2023-04-03 09:41:59 -0400

    Midterm

---
+++ Midterm/script.js
@@ -0,0 +1,27 @@
+
+         let hour = 13;
+         if (hour >= 6 && hour < 12){
+            document.write('Good morning');
+         }
+         else if (hour >= 12 && hour < 18) {
+            document.write('Good afternoon');
+         } 
+         else {
+            document.write('Good evening');
+         }
+
+
+ function gotoLink(link){
+               console.log(link.value);
+               location.href = link.value;
+            };
+
+
+
+console.log(window.innerWidth)
+document.write("Current width of screen");
+document.write(innerWidth);
+
+
+
+ 
